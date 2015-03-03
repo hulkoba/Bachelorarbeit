ddefde64809bfa9314fc9cc1eadede65cd2b2afb
hulkoba <jacoba.brandner@web.de>
2015-03-03 17:19:10 +0100

    implementierung

--- code/gpstracker.java
+++ code/gpstracker.java
@@ -10,12 +10,17 @@ private void getNearestLSA(Location myLocation){
     float minDistance = Float.MAX_VALUE;
 
 // Ampeln in der Umgebung suchen
+// wurden schon Ampeln gespeichert?
 if (nearestLSAs == null ) {            
     nearestLSAs = new ArrayList<LSA>();
 
     for (LSA lsa : lsas) {
+    	// fuer saemtliche Ampeln die Distanz errechnen
         distance = myLocation.distanceTo(lsa.getLsaLocation());
-        if (distance <= Constants.MIN_LSA_DISTANCE) {
+        
+        // ist die Distanz im Radius?
+        if (distance <= Constants.MIN_LSA_DISTANCE) {            
+            // Ampel mit Distanz speichern
             LSA nlsa = new LSA(distance, 
                	lsa.getName(), 
                	lsa.getLsaLocation(), 
@@ -24,14 +29,20 @@ if (nearestLSAs == null ) {
             nearestLSAs.add(nlsa);
         }
     }
+
 // Ampeln in der Umgebung gefunden, noch keine Ampel festgelegt
 } else if(nearestLSAs != null && nearestLSA == null) {          
     for(LSA lsa : nearestLSAs){
+    	// fuer jede Ampel in der Umgebung Distanz errechnen
         distance = myLocation.distanceTo(lsa.getLsaLocation());
+
+        // verkleinert sich Distanz im Gegensatz zur Vorherigen?
+        // Ist die Distanz von allen nahem LSAs am geringsten? 
         if (distance < lsa.getDistance() 
         && distance <= Constants.MIN_LSA_DISTANCE  
         && minDistance > distance){ 
             minDistance = distance;
+        	// naechste Ampel ermittelt!
             nearestLSA = lsa;    
             if(onSetListener != null){
             // LSA gefunden?  per Listener MainActivity benachrichtigen              
@@ -41,10 +52,13 @@ if (nearestLSAs == null ) {
     }   
 }
 
-//Entfernung ist hoeher als geg. Distanz oder Entfernung ist groesser als vorher
+// Entfernung ist hoeher als gegebene Distanz?
+// und Entfernung ist groesser als vorher
 if(nearestLSA != null) {
     if(myLocation.distanceTo(nearestLSA.getLsaLocation()) > Constants.MIN_LSA_DISTANCE 
       	|| myLocation.distanceTo(nearestLSA.getLsaLocation()) > (distance+0.1)) {
+        
+        // Ampeln und temporaere Liste loeschen
         nearestLSAs = null;
       	nearestLSA = null;
     }
